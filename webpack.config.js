0fe1723ff4e3ddd8944697766c033afc9a79198d
ilyass atlassi <atlassiilyass@gmail.com>
2023-03-08 14:52:21 +0100

    Add webpack and linters packages

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable  */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+  mode: 'development',
+   entry: {
+     index: './src/index.js', 
+   },
+
+   devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+   optimization: {
+    runtimeChunk: 'single',
+  },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+ };
