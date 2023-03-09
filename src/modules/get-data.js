2af40c79d84534e42ed69fd66743928bc6ef0b66
ilyass atlassi <atlassiilyass@gmail.com>
2023-03-09 14:54:46 +0100

    Add the get method

--- src/modules/get-data.js
+++ src/modules/get-data.js
@@ -1,1 +1,17 @@
-const api = 
+const api = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/"
+
+const getData = async () => {
+    const response = await fetch(api);
+    const data = await response.json();
+    printData(data)
+}
+
+const printData = async (data) => {
+    const score = document.querySelector("#result")
+    data.result.forEach((element) => {
+        score.innerHTML += `
+        <p>${element.user} : ${element.score}</p>`
+    });
+}
+
+getData()
